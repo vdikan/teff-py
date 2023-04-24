02f3b2d5338b7c0ff9888e8078c115f819b03ee4
Vladimir Dikan <vdikan@vivaldi.net>
2023-04-24 20:36:05 +0200

    init utility collection for TDEP

---
+++ teff_py/tdep_utils.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+from plumbum import local
+from plumbum.cmd import grep, awk, tail
+
+
+def get_overdetermination_report(fname):
+    """Obtain data on the grade of FCs equations system overdetermination
+    from the `extract_forceconstants` output file `fname`.
+
+    Result: {fc_order: (num_fcs_upto_this_order, overdetermination_grade), ...}
+    """
+    results = {}
+    for fc in [2, 3, 4]:
+        pipe = grep["-A4", "REPORT GRADE OF OVERDETERMINATION", fname] | \
+            grep[f"up {fc}. order"] | \
+            awk['{ print $7, $11 }']
+        res = pipe(retcode=None).strip()
+        if len(res) > 0:
+            num, ratio = res.split()
+            results[fc] = (int(num), float(ratio))
+
+    return results
+
+
+def get_r_squared(fname):
+    """Obtain `R-squared` coefficient of determination for the fit
+    from `extract_forceconstants` output file `fname`.
+
+    Result: {fc_order: r_squared, ...}"""
+    results = {}
+    for fc, label in [(2, "second"),
+                      (3, "third"),
+                      (4, "fourth")]:
+        pipe = grep["-A4", "R^2", fname] | \
+            grep[f"{label} order"] | \
+            awk['{ print $3 }']
+        res = pipe(retcode=None).strip()
+        if len(res) > 0:
+            r_squared = float(res)
+            results[fc] = r_squared
+
+    return results
+
+
+def get_interactions(fname):
+    """Read number of shells and forceconstants
+    from `extract_forceconstants` output file `fname`.
+
+    Result: {fc_order: (num_shells, num_fcs), ...}
+    """
+    results = {}
+    for fc, label in [(1, "first"),
+                      (2, "second"),
+                      (3, "third"),
+                      (4, "fourth")]:
+        pipe = grep["-A4", "Interactions:", fname] | \
+            grep[f"{label}order forceconstant:"] | \
+            awk['{ print $3, $4 }']
+        res = pipe(retcode=None).strip().split()
+        results[fc] = (res[0], res[1])
+
+    return results
+
+
+def get_elastic_constants(fname):
+    """Read elastic constants matrix in a `numpy` format
+    from `extract_forceconstants` output file `fname`.
+    """
+    pipe = grep["-A6", "elastic constants", fname] | tail[-6]
+    data = ";".join(pipe().strip().split("\n"))
+
+    return np.matrix(data, dtype=float)
